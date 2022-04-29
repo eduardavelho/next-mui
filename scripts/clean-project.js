93548b84e499f84c0b8fd82df241a5ee1c69f235
Eduardo Velho <eduardo.velho@icloud.com>
2022-04-29 19:45:34 -0300

    script fix

--- scripts/clean-project.js
+++ scripts/clean-project.js
@@ -5,7 +5,7 @@ const { exec } = require("child_process");
 console.log("Deleting build files...");
 
 exec(
-  "rm -rf *.d.ts *.js *.js.map blog-page dash info-page page post-page section social-page components utils typography storybook-static",
+  "rm -rf *.d.ts *.js *.js.map account blog-page dash info-page page post-page section social-page components utils typography storybook-static",
   () => {
     console.log("Project cleaned with success!");
   }
