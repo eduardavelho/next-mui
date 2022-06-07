8f97ebd759ddad6a5148372dd95c76d731e7ba9c
Eduardo Velho <eduardo.velho@icloud.com>
2022-06-06 21:42:41 -0300

    viewport fix

--- src/utils/use-viewport.ts
+++ src/utils/use-viewport.ts
@@ -3,10 +3,11 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export function useViewport() {
   const theme = useTheme();
-  const isPhone = useMediaQuery(theme.breakpoints.down("sm"));
-  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
-  const isDesktop = useMediaQuery(theme.breakpoints.up(1600));
-  const isLaptop = !isDesktop && useMediaQuery(theme.breakpoints.up("md"));
+  const useIsPhone = () => useMediaQuery(theme.breakpoints.down("sm"));
+  const useIsTablet = () => useMediaQuery(theme.breakpoints.down("md"));
+  const useIsLaptop = () =>
+    useMediaQuery(theme.breakpoints.between("md", 1600));
+  const useIsDesktop = () => useMediaQuery(theme.breakpoints.up(1600));
 
-  return { isPhone, isTablet, isLaptop, isDesktop };
+  return { useIsPhone, useIsTablet, useIsLaptop, useIsDesktop };
 }
