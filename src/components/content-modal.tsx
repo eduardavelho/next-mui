d9c9c2955e62d4df2a433e0dfadfcc373727f8b2
Eduardo Velho <eduardo.velho@icloud.com>
2022-06-14 18:23:34 -0300

    fix

--- src/components/content-modal.tsx
+++ src/components/content-modal.tsx
@@ -6,7 +6,6 @@ import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogActions from "@material-ui/core/DialogActions";
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
